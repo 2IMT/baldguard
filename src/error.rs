03c4b42c6802af0a37053b2a03ae49039f3afeab
Daniil Tiuftin <daniil_t04@mail.ru>
2024-11-21 22:42:52 +0300

    Add GenericError type

---
+++ src/error.rs
@@ -0,0 +1,18 @@
+use std::fmt::Display;
+
+#[derive(Clone, Debug)]
+pub struct GenericError {
+    message: String,
+}
+
+impl From<String> for GenericError {
+    fn from(message: String) -> Self {
+        GenericError { message }
+    }
+}
+
+impl Display for GenericError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
